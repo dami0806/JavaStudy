1c2555b3ee39b14ea36be2882626602f718eed9d
dami0806 <dami0804@naver.com>
2024-04-23 17:24:51 +0900

    week_03: abstract
    추상화전 중복된 코드 확인

---
+++ JavaStudy/src/week_03/abs/BenzCar.java
@@ -0,0 +1,22 @@
+package week_03.abs;
+
+public class BenzCar {
+    String company; // 자동차 회사 : GENESIS
+    String color; // 자동차 색상
+    double speed;  // 자동차 속도 , km/h
+
+    public double gasPedal(double kmh) {
+        speed = kmh;
+        return speed;
+    }
+
+    public double brakePedal() {
+        speed = 0;
+        return speed;
+    }
+
+    public void horn() {
+        System.out.println("Benz 빵빵");
+    }
+
+}
