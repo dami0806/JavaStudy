ca36352a95933d5c7b98241e58f1f3c33abf43f5
dami0806 <dami0804@naver.com>
2024-04-23 12:03:45 +0900

    week_03: package
    import 사용 후, 다른 package 동명객체 -> 나머지는 경로로명시

--- JavaStudy/src/week_03/packageExample/main/Main.java
+++ JavaStudy/src/week_03/packageExample/main/Main.java
@@ -1,13 +1,10 @@
 package week_03.packageExample.main;
+import week_03.packageExample.pk1.Car;
 
 public class Main {
     public static void main(String[] args) {
-        // Car car = new Car(); 다른패키지에 있어서 불가
-
-        // 클래스의 일부분이며, 하위 패키지를 도트(.)로 구분한다.
-            // import week_03.packageExample.pk1.Car는 다음 코드에서
-        week_03.packageExample.pk1.Car car1 = new week_03.packageExample.pk1.Car();
-        week_03.packageExample.pk2.Car car2 = new week_03.packageExample.pk2.Car();
+         Car car1 = new Car();
+         week_03.packageExample.pk2.Car car2 = new week_03.packageExample.pk2.Car();
 
         car1.horn();
         car2.horn();
