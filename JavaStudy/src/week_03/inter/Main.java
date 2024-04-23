30b2b77cfe9bdaf98c8ddfa20441c21896da936c
dami0806 <dami0804@naver.com>
2024-04-23 17:51:33 +0900

    week_03: interface

---
+++ JavaStudy/src/week_03/inter/Main.java
@@ -0,0 +1,44 @@
+package week_03.inter;
+
+public class Main extends D implements C {
+
+    // c에 상속받은 A,B
+    @Override
+    public void a() {
+        System.out.println("A");
+    }
+
+    @Override
+    public void b() {
+        System.out.println("B");
+    }
+
+    @Override
+    void d() {
+        super.d();
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.a();
+        main.b();
+        main.d();
+    }
+}
+
+interface A {
+    void a();
+}
+
+interface B {
+    void b();
+}
+
+interface C extends A, B {
+}
+
+class D {
+    void d() {
+        System.out.println("D");
+    }
+}
