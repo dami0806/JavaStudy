93bc67666d15dd8ca2c45fca53bed3940a8aa86c
dami0806 <dami0804@naver.com>
2024-04-23 17:31:30 +0900

    week_03: abstract
    추상화 클래스

--- JavaStudy/src/week_03/abs/AudiCar.java
+++ JavaStudy/src/week_03/abs/AudiCar.java
@@ -1,20 +1,7 @@
 package week_03.abs;
 
-public class AudiCar {
-    String company; // 자동차 회사 : GENESIS
-    String color; // 자동차 색상
-    double speed;  // 자동차 속도 , km/h
-
-    public double gasPedal(double kmh) {
-        speed = kmh;
-        return speed;
-    }
-
-    public double brakePedal() {
-        speed = 0;
-        return speed;
-    }
-
+public class AudiCar extends Car{
+    @Override
     public void horn() {
         System.out.println("Audi 빵빵");
     }
