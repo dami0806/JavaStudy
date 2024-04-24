47e9710b8e02a29afa966ba149a3586156e313e3
dami0806 <dami0804@naver.com>
2024-04-24 10:42:28 +0900

    Test_week_03 : Calculator
    step 01

---
+++ JavaStudy/src/week_03_calculator/step1/Calculator.java
@@ -0,0 +1,35 @@
+package week_03_calculator.step1;
+
+/*
+<aside>
+1️⃣ 더하기, 빼기, 나누기, 곱하기 연산을 수행할 수 있는 Calculator 클래스를 만듭니다.
+
+- Calulator 클래스는 연산을 수행하는 반환 타입이 double인 calculate 메서드를 가지고 있습니다.
+- calculate 메서드는 String 타입의 operator 매개변수를 통해 연산자 매개값을 받습니다.
+- int 타입의 firstNumber, secondNumber 매개변수를 통해 피연산자 값을 받습니다.
+- calculate 메서드는 전달받은 피연산자, 연산자를 사용하여 연산을 수행합니다.
+ */
+public class Calculator {
+
+    // 메서드
+    double calculate(String operator, int firstNumber, int secondNumber) throws IllegalArgumentException {
+        // 더하기, 빼기, 나누기, 곱하기
+        switch (operator) {
+            case "+":
+                return firstNumber + secondNumber;
+            case "-":
+                return firstNumber - secondNumber;
+            case "*":
+                return firstNumber * secondNumber;
+            case "/":
+                if (secondNumber == 0) {
+                    throw new IllegalArgumentException("0으로 나눌 수 없습니다.");
+                }
+                return (double)firstNumber / secondNumber;
+            default:
+                throw new IllegalArgumentException("유효하지 않은 연산자 입니다.");
+        }
+
+    }
+
+}
---
+++ JavaStudy/src/week_03_calculator/step1/Main.java
@@ -0,0 +1,40 @@
+package week_03_calculator.step1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            String operator;
+            int firstNumber, secondNumber;
+
+            System.out.println("연산자를 입력하세요 (+, -, *, /): ");
+            operator = br.readLine();
+
+            System.out.println("firstNumber를 적어주세요: ");
+            firstNumber = Integer.parseInt(br.readLine());
+
+            System.out.println("secondNumber를 적어주세요: ");
+            secondNumber = Integer.parseInt(br.readLine());
+
+
+            Calculator cal = new Calculator();
+            double answer = cal.calculate(operator, firstNumber, secondNumber);
+
+            System.out.printf("%d %s %d = %.2f", firstNumber, operator, secondNumber, answer);
+
+        } catch (IOException e) {
+            System.out.println("입력 에러 발생: " + e.getMessage());
+        }
+        catch (NumberFormatException e) {
+            System.out.println("숫자 입력 에러 발생: " + e.getMessage());
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println("계산에 에러가 발생했습니다." + e.getMessage());
+
+        }
+    }
+}
