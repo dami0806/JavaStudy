c54ca4fc92cb82cb75ff76dd24249b2d5049f390
dami0806 <dami0804@naver.com>
2024-04-25 12:42:34 +0900

    week_05: Thread
    Thread, Runnable Thread

---
+++ JavaStudy/src/week_05/Main.java
@@ -0,0 +1,16 @@
+package week_05;
+
+public class Main {
+    public static void main(String[] args) {
+        // 쓰레드
+//        TestThread thread = new TestThread();
+//        thread.start();
+        //Runnable 을 더 선호
+        Runnable run = new TestRunnable();
+        Thread thread = new Thread(run);
+        thread.start();
+
+
+    }
+}
+
