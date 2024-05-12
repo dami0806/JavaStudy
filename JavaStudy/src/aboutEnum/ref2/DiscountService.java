53af457d2682238781ef8a4cf8ac3ca8caebd642
dami0806 <dami0804@naver.com>
2024-05-12 20:41:07 +0900

    refactor: enum
    clean code

--- JavaStudy/src/aboutEnum/ref2/DiscountService.java
+++ JavaStudy/src/aboutEnum/ref2/DiscountService.java
@@ -1,7 +1,7 @@
 package aboutEnum.ref2;
 
-public class DiscountService {
-    public int discount(Grade classGrade, int price) {
-        return price * classGrade.getDiscountPercent() / 100;
-    }
-}
+//public class DiscountService {
+//    public int discount(Grade classGrade, int price) {
+//        return classGrade.disount(price);
+//    }
+//}
--- JavaStudy/src/aboutEnum/ref2/Grade.java
+++ JavaStudy/src/aboutEnum/ref2/Grade.java
@@ -1,10 +1,13 @@
 package aboutEnum.ref2;
 
 public enum Grade {
-    BASIC(10), GOLD(20), DIAMOND(30);
+    BASIC(10), GOLD(20), DIAMOND(30), VIP(40);
 
-    public int getDiscountPercent() {
-        return discountPercent;
+    //    public int getDiscountPercent() {
+//        return discountPercent;
+//    }
+    public int disount(int price) {
+        return price * discountPercent / 100;
     }
 
     Grade(int discountPercent) {
