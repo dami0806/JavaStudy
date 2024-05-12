6b15eeb7f7ce23c03ba109a1d35f3ea00d20557a
dami0806 <dami0804@naver.com>
2024-05-12 19:59:14 +0900

    Enum
    주요 메서드

---
+++ JavaStudy/src/aboutEnum/ex3/EnumMethodMain.java
@@ -0,0 +1,18 @@
+package aboutEnum.ex3;
+
+import java.util.Arrays;
+
+public class EnumMethodMain {
+    public static void main(String[] args) {
+        // 모든 Enum 반환
+        Grade[] values = Grade.values();
+        System.out.println("values = " + Arrays.toString(values));
+        for (Grade value : values) {
+            System.out.println("name" + value.name() + ", ordinal = " + value.ordinal());
+        }
+        // String -> Enum 변환
+        String input = "GOLD";
+        Grade grade = Grade.valueOf(input);
+        System.out.println("gold = " + grade);
+    }
+}
